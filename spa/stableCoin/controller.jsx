4fea11c5eaeca3a5cd5d207eb00106d516317341
Marco Vasapollo <ceo@metaring.com>
2020-08-27 19:13:24 +0200

    Disabled Pairs

--- spa/stableCoin/controller.jsx
+++ spa/stableCoin/controller.jsx
@@ -39,24 +39,30 @@ var StableCoinController = function (view) {
                 token0: await window.loadTokenInfos(await window.blockchainCall(pair.methods.token0)),
                 token1: await window.loadTokenInfos(await window.blockchainCall(pair.methods.token1))
             };
+            pairData.name = pairData.token0.symbol + ' / ' + pairData.token1.symbol;
             var reserves = await window.blockchainCall(pair.methods.getReserves);
             reserves[0] = context.fromTokenToStable(pairData.token0.decimals, reserves[0]);
             reserves[1] = context.fromTokenToStable(pairData.token1.decimals, reserves[1]);
             var total = window.web3.utils.toBN(reserves[0]).add(window.web3.utils.toBN(reserves[1])).toString();
             total = window.fromDecimals(total, window.stableCoin.decimals, true);
             total = parseFloat(total);
-            console.log(total);
             if(total < window.context.uSDPoolLimit) {
-                console.log("disabled");
-                pair.disabled = true;
+                pairData.disabled = true;
             }
-            pairData.name = pairData.token0.symbol + ' / ' + pairData.token1.symbol;
             pairs.push(pairData);
         }
-        context.view.setState({ pairs, selectedPair: pairs[0], token0Approved: null, token1Approved: null }, function () {
-            context.checkApprove(pairs[0]);
+        context.view.setState({ pairs, selectedPair: context.firstNonDisabledPair(pairs), token0Approved: null, token1Approved: null }, function () {
+            context.checkApprove(context.view.state.selectedPair);
             context.getTotalCoins();
         });
+    };
+
+    context.firstNonDisabledPair = function firstNonDisabledPair(pairs) {
+        for(var pairData of pairs) {
+            if(!pairData.disabled) {
+                return pairData;
+            }
+        }
     };
 
     context.getBalance = async function getBalance(selectedPair) {
